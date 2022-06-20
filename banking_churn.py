60f4bc3ac86da93ec60ed37ce222a3d25b04dcc0
Charles Livuza <36395099+Livuza@users.noreply.github.com>
2022-06-20 14:00:36 +0300

    Update banking_churn.py

--- banking_churn.py
+++ banking_churn.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 url = "https://raw.githubusercontent.com/Livuza/ADS-April-2021/main/Assignments/Assignment%202/banking_churn.csv"
